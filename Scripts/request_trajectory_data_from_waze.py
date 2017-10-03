a55d8224a24f31a5982cb4b1c2e021bce07fba82
carloscuzik <cuzikjack@gmail.com>
2017-10-03 15:54:20 -0300

    colocando o arquivo do pdf ainda errado

--- Scripts/request_trajectory_data_from_waze.py
+++ Scripts/request_trajectory_data_from_waze.py
@@ -1,13 +1,40 @@
 import json, requests
 
-names_ids = {}
-names_ids.update({'RockFeller':'ChIJkdEaSj-w3pQRUhf00nVYu5g'})
-names_ids.update({'Magrathea_Labs':'ChIJG6B5DC6w3pQRHM-7HtOV7lU'})
+locals_address = {}
+locals_address.update({'RockFeller'     : 'ChIJkdEaSj-w3pQRUhf00nVYu5g'})
+locals_address.update({'Magrathea_Labs' : 'ChIJG6B5DC6w3pQRHM-7HtOV7lU'})
 
-def rote_info(origin, destination):
-	return requests.get("https://maps.googleapis.com/maps/api/directions/json?"
-											+ "origin=place_id:" + names_ids[origin]
-											+ "&destination=place_id:" + names_ids[destination]
-											+ "&key=AIzaSyDNFfzulwTdNST376L6sCPaMtvCBYNPbR0").content
+my_key = {}
+my_key.update({'directions'     : 'AIzaSyDNFfzulwTdNST376L6sCPaMtvCBYNPbR0'})
 
-print rote_info('RockFeller','Magrathea_Labs')
+api_request = {}
+api_request.update({'directions':'directions'})
+
+forma_request = {}
+forma_request.update({'json' : 'json'})
+forma_request.update({'xml'  : 'xml' })
+
+mode = {}
+mode.update({'car' : 'driving'})
+mode.update({'bus' : 'transit'})
+
+def rote_info(origin, destination,mode_go,api_mode,return_format):
+	require_url = ("https://maps.googleapis.com/maps/api/"
+											+ api_request[api_mode]
+											+ '/' + forma_request[return_format] +'?'
+											+ "origin=place_id:" + locals_address[origin]
+											+ "&destination=place_id:" + locals_address[destination]
+											+ "&mode=" + mode[mode_go]
+											+ "&key=" + my_key[api_mode])
+	
+	content = requests.get(require_url).content
+	json_content = json.loads(content)
+
+	json_routs = json_content['routes'][0]
+
+	json_legs = json_routs['legs'][0]
+	
+	for step in json_legs['steps']:
+		print str(float(step['distance']['value']) / float(step['duration']['value'])) + ':' + step['html_instructions']
+
+print rote_info('RockFeller','Magrathea_Labs','car','directions','json')
