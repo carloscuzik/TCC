528d1571d2abb235a87f3df791bd3a95ed034ce7
carloscuzik <cuzikjack@gmail.com>
2017-10-10 20:35:36 -0300

    colocando os erros

--- Scripts/request_information_about_streets.py
+++ Scripts/request_information_about_streets.py
@@ -12,24 +12,24 @@ def street_info(street_name, city_name,my_local_id):
 	json_content = json.loads(json.dumps(xmltodict.parse(content)))['PlaceSearchResponse']
 	
 	if json_content.has_key('result') == False:
-		return '{"not find"}'
+		return '{"error_type":"not find","name":"' + city_name + '", "url":"' + require_url + '"}'
 
 	if type(json_content['result']) == dict:
 		json_results = json_content['result']
 	else:
-		return {'many results'}
+		return '{"error_type":"many results","name":"' + city_name + '", "url":"' + require_url + '"}'
 		# json_results = json_content['result'][0]
 
-	util_street_info.update({'my_local_id':my_local_id})
-	util_street_info.update({'google_id':json_results['id']})
-	util_street_info.update({'name':json_results['name']})
-	util_street_info.update({'place_id':json_results['place_id']})
-	util_street_info.update({'geometry':{'lat':json_results['geometry']['location']['lat'],'lng':json_results['geometry']['location']['lng']}})
+	# util_street_info.update({'my_local_id':my_local_id})
+	# util_street_info.update({'google_id':json_results['id']})
+	# util_street_info.update({'name':json_results['name']})
+	# util_street_info.update({'place_id':json_results['place_id']})
+	# util_street_info.update({'geometry':{'lat':json_results['geometry']['location']['lat'],'lng':json_results['geometry']['location']['lng']}})
 
 	return util_street_info
 
 file_in = open('../street_names','r')
-file_out = open('../street_info_from_google','w')
+file_out = open('../street_info_from_google_error','w')
 count = 0
 for line in file_in:
 	count +=1
